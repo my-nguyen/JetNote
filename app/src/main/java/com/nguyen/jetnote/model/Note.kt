e5b54af1afb40c7521d75258d703c22394329a87
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-03 15:58:45 -0700

    b10: creating a Note entity

--- app/src/main/java/com/nguyen/jetnote/model/Note.kt
+++ app/src/main/java/com/nguyen/jetnote/model/Note.kt
@@ -2,12 +2,18 @@ package com.nguyen.jetnote.model
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 import java.util.UUID
 
+@Entity(tableName = "notes_tbl")
 data class Note @RequiresApi(Build.VERSION_CODES.O) constructor(
+    @PrimaryKey
     val id: UUID = UUID.randomUUID(),
     val title: String,
     val description: String,
+    @ColumnInfo("entry_date")
     val entryDate: LocalDateTime = LocalDateTime.now()
 )
