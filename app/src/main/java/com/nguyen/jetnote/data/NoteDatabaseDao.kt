cb90414c9b187fc658008cfb3ddc697233b7d95a
Mỹ Nguyễn <nguyen@MacBook-Pro-M1Max-32G-1T.local>
2025-10-03 16:09:59 -0700

    b12: adding Flow state to getAllNotes

--- app/src/main/java/com/nguyen/jetnote/data/NoteDatabaseDao.kt
+++ app/src/main/java/com/nguyen/jetnote/data/NoteDatabaseDao.kt
@@ -7,25 +7,25 @@ import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import androidx.room.Update
 import com.nguyen.jetnote.model.Note
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface NoteDatabaseDao {
     @Query("SELECT * from notes_tbl")
-    fun selectAll(): List<Note>
+    fun getAllNotes(): Flow<List<Note>>
 
     @Query("SELECT * from notes_tbl where id =:id")
-    suspend fun selectById(id: String): Note
+    suspend fun getNoteById(id: String): Note
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insert(note: Note)
+    suspend fun insertNote(note: Note)
 
     @Update(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun update(note: Note)
+    suspend fun updateNote(note: Note)
 
     @Query("DELETE from notes_tbl")
-    suspend fun deleteAll()
+    suspend fun deleteAllNotes()
 
     @Delete
-    suspend fun delete(note: Note)
-
+    suspend fun deleteNote(note: Note)
 }
